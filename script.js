e6265058ff15e334bbe2eb606fbfc4eb1548a2b0
maruf-ahmed <marufahamed1110@gmail.com>
2024-02-29 13:14:48 +0600

    Added dynamic card

--- script.js
+++ script.js
@@ -6,10 +6,58 @@ const fetchCategory = async () => {
   let data = await res.json();
   let categoryData = data.data;
   categoryData.map(function (element) {
+    // let elementId = element.category_id;
+    // console.log(elementId);
     let button = document.createElement("button");
     button.className = "btn bg-red-500 text-white hover:bg-red-400 mx-1";
     button.innerText = element.category;
     buttonContainer.appendChild(button);
+    button.addEventListener("click", () =>
+      fetchDataByCategory(element.category_id)
+    );
   });
 };
+
+const fetchDataByCategory = async (elementId) => {
+  console.log(elementId);
+  let res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${elementId}`
+  );
+  let data = await res.json();
+  let allData = data.data;
+  allData.map(function (element) {
+    console.log(element);
+    let cardContainer = document.getElementById("cards-container");
+    let cardDiv = document.createElement("div");
+    cardDiv.innerHTML = `
+      <div class="card  bg-base-100 shadow-md">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div class="flex gap-4 py-6 px-2">
+          <div class="">
+            <img
+              class="rounded-full w-24"
+              src="assets/Noha.jpg"
+              alt=""
+              srcset=""
+            />
+          </div>
+          <div class="">
+            <h2 class="card-title">
+              Building a Winning UX Strategy Using the Kano Model
+            </h2>
+            <p>Awlad Hossain</p>
+            <p class="text-gray-600">91K views</p>
+          </div>
+        </div>
+      </div>
+      `;
+    cardContainer.appendChild(cardDiv);
+  });
+};
+
 fetchCategory();
